f31ddf3e3e9dfbb52c0d9a051668efd12c7498c9
midstreamer <chadalessi@yahoo.com>
2018-06-19 06:54:52 -0500

    added homework files from the working directory

---
+++ main.js
@@ -0,0 +1,41 @@
+var game = require("./game.js");
+var wordCons = require("./word.js");
+var letterCons = require("./letter.js");
+var inquirer = require("inquirer");
+var randoWord = game.randoWord;
+var letterGuessed;
+exports.letter;
+
+var myWord = new wordCons.wordCons(game.randoWord);
+var maxGuesses = 15;
+
+function takeAGuess(){
+	console.log(myWord.toString());
+	if (myWord.guessesMade.length >= maxGuesses){
+		console.log('You have reached the maximum number of guess. Game Over!');
+	return; //Game over
+	}
+	inquirer.prompt([{
+		name: 'letter',
+		type: 'text',
+		message: 'Enter a letter:',
+		validate: function(str){
+//			if (str.length != 1) return false;
+			var regEx = new RegExp("^[a-zA-Z\s]{1,1}$");
+			return regEx.test(str);
+				}
+		}]).then(function(letterInput){ //Game control
+				var letter = letterInput.letter; 
+				myWord.findLetter(letter); //Check
+					if(myWord.isComplete()){ 
+					console.log('Congratulations! It was ' + myWord.toString() + '!');
+					return; //Winner
+					}
+				console.log('-------------------\n'); //If we are here the game did not end. Next guess.
+				console.log('You have ' + (maxGuesses - myWord.guessesMade.length) + ' guesses left.')
+				takeAGuess(); //Recursive call
+				}
+  );
+}
+
+takeAGuess(); //Start Game
